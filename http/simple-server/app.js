1cb8d9ac58160fc52a580ae885a526379cacc7a2
Nikhit Adusumilli <nikhitadusumilli@gmail.com>
2025-08-21 01:12:04 +0530

    Changes to understand how the data is transferred between clients

---
+++ http/simple-server/app.js
@@ -0,0 +1,29 @@
+const http = require("node:http");
+
+const server = http.createServer();
+
+// req is a readable stream, and response is a writable stream.
+server.on("request", (req, res) => {
+    const name = req.headers.name;
+
+    if (req.method === "POST" && req.url === "/create-post") {
+        let data;
+        req.on("data", (chunk) => {
+            data += chunk.toString()
+        })
+
+        req.on("end", () => {
+            data = JSON.parse(data.substring(9));
+            console.log("reached the ned.")
+            console.log(data);
+
+
+            res.writeHead(200, { "Content-Type": "application/json" })
+            res.end(JSON.stringify({ message: `Post with data ${data.title} was created by ${name}` }))
+        })
+    }
+})
+
+server.listen(8050, () => {
+    console.log("server started on http://localhost:8050")
+})
